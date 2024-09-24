0ec0dea3a4061d5c3a3e422499c55fbc0bfee69b
Justin Kruskie <justin@kruskie.net>
2024-09-24 19:32:50 -0400

    Create Payroll.java

---
+++ Payroll.java
@@ -0,0 +1,67 @@
+// Create a public class named Payroll and design the class based on
+// the comments found below. Block comments indicate a new method.
+
+  // Create a constant integer named NUM_ASTRONAUTS and set it to 7.
+
+  // Create an integer array called astronautId and give it the values:
+    // 5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 7580489
+
+    // Array to hold hours worked as integers the size of NUM_ASTRONAUTS
+
+    // Array to hold pay rates as doubles the size of NUM_ASTRONAUTS
+
+    // Array to hold gross wages as doubles the size of NUM_ASTRONAUTS
+
+    /*
+    The getGrossPay method returns the gross pay for the astronaut
+    whose data is stored at a specified element of each array.
+    @param i The specified element.
+    @return The value stored at the specified element.
+    */
+
+
+    /*
+    The setAstronautIdAt method stores an ID number at a specified 
+    element of the astronautId array.
+    @param i The specified element.
+    @param id The ID number to store.
+    */
+
+
+    /*
+    The setHoursAt method stores a number of hours at a specified 
+    element of the hours array.
+    @param i The specified element.
+    @param h The number of hours to store.
+    */
+
+
+    /*
+    The setPayRateAt method stores a pay rate at a specified 
+    element of the payRate array.
+    @param i The specified element.
+    @param p The pay rate to store.
+    */
+
+
+    /*
+    The getAstronautIDAt method returns the ID number at a 
+    specified element of the astronautId array.
+    @param i The specified element.
+    @return The ID number found at element i.
+    */
+
+    /*
+    The getHoursAt method returns the hours found at a specified 
+    element of the hours array.
+    @param i The specified element.
+    @return The hours found at element i.
+    */
+
+
+    /*
+    The getPayRateAt method returns the pay rate found at a 
+    specified element of the payRate array.
+    @param i The specified element.
+    @return The pay rate found at element i.
+    */
